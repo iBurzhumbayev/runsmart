dd43ca552d0d6e830c9962fbaf68be31bc4c6ea5
Ilyas <kz.smile@bk.ru>
2022-07-01 05:25:19 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1500,
+        /* adaptiveHeight: true, */
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+    });
+  });
